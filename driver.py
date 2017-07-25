4f54c5ac5c65396868acb6b7f322a06dbb796fc8
Maded Batara III <mnbatara3@gmail.com>
2017-07-25 10:16:08 +0800

    Added DRIVER scraper

---
+++ driver.py
@@ -0,0 +1,55 @@
+import csv
+import requests
+
+# Set to true to output in CSV, false to output in JSON
+OUTPUT_IN_CSV = True
+
+headers = {
+    "Authorization": "Token adc7bbce935c6b230d40449de40f05d387e78128"
+}
+
+get = requests.get("https://roadsafety.gov.ph/api/records/", headers=headers)
+csv_fieldnames = [
+    "uuid",
+    "location",
+    "lat",
+    "long",
+    "time",
+    "description",
+    "agency",
+    "cause",
+    "collision_type",
+    "severity",
+    "city",
+    "district",
+    "neighborhood",
+    "road",
+    "weather",
+    "light"
+]
+
+with open("driver.{0}".format("csv" if OUTPUT_IN_CSV else "json"), "w") as driver_file:
+    if OUTPUT_IN_CSV:
+        writer = csv.DictWriter(driver_file, fieldnames=csv_fieldnames, lineterminator='\n')
+        writer.writeheader()
+        while get.json()["next"] is not None:
+            for result in get.json()["results"]:
+                data = {}
+                data["uuid"] = result["uuid"]
+                data["location"] = result["location_text"]
+                data["lat"] = result["geom"]["coordinates"][1]
+                data["long"] = result["geom"]["coordinates"][0]
+                data["time"] = result["occurred_from"]
+                data["description"] = result["data"]["incidentDetails"].get("Description")
+                data["agency"] = result["data"]["incidentDetails"].get("Reporting Agency")
+                data["cause"] = result["data"]["incidentDetails"].get("Main cause")
+                data["collision_type"] = result["data"]["incidentDetails"].get("Collision type")
+                data["severity"] = ','.join(result["data"]["incidentDetails"]["Severity"])
+                data["city"] = result["city"]
+                data["district"] = result["county"]
+                data["neighborhood"] = result["neighborhood"]
+                data["road"] = result["road"]
+                data["weather"] = result["weather"]
+                data["light"] = result["light"]
+                writer.writerow(data)
+            get = requests.get(get.json()["next"], headers=headers)
